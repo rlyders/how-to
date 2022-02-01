909562265a51e352263ec782eaa38c58bf225c6f
RJLyders <richard@lyders.com>
2022-01-31 20:04:18 -0600

    fix: update github ref in gh-pages.js fix: update href to use %svelte.assets%

--- gh-pages.js
+++ gh-pages.js
@@ -4,10 +4,10 @@ publish(
  'build', // path to public directory
  {
   branch: 'gh-pages',
-  repo: 'https://github.com/el3um4s/memento-sveltekit-and-github-pages.git', // Update to point to your repository
+  repo: 'https://github.com/rlyders/how-to.git', // Update to point to your repository
   user: {
-   name: 'Samuele de Tomasi', // update to use your name
-   email: 'samuele@stranianelli.com' // Update to use your email
+   name: 'Richard Lyders', // update to use your name
+   email: 'Richard@Lyders.com' // Update to use your email
   },
   dotfiles: true
   },
