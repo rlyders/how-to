36ecceff1b7f9bbf589ac6640581d322be8e31de
RJLyders <richard@lyders.com>
2022-02-02 20:12:35 -0600

    upgraded to SvelteKit v1.0.0-next.259

--- svelte.config.js
+++ svelte.config.js
@@ -21,10 +21,6 @@ const config = {
 		// 	fallback: null,
 		// 	precompress: false
 		// }),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
 		vite: {
 			plugins: [yaml()]
 		}
